bece9b250fc8bfa5e1828bfb45d45e825707977e
pothuganti <pothugantisruthi8@gmail.com>
2026-09-06 18:40:18 +0530

    leetcode 33

---
+++ 33.py
@@ -0,0 +1,19 @@
+nums = [4,5,6,7,0,1,2]
+target = 0
+l=0
+r=len(nums)-1
+while l<=r:
+    mid=(l+r)//2
+    if target==nums[mid]:
+        print(mid)
+        break
+    if nums[l]<=nums[mid]:
+        if nums[l]<=target<nums[mid]:
+            r=mid-1
+        else:
+            l=mid+1
+    else:
+        if nums[l]<=target<nums[mid]:
+            l=mid+1
+        else:
+            r=mid-1
